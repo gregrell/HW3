e0ba50eb33a3e12971c65e131347181eea9087c1
Greg Rell <rell.greg@gmail.com>
2019-11-03 11:32:25 +0800

    Ran for all docs. Summaries in spreadsheet, not finalized

--- combineEvals.py
+++ combineEvals.py
@@ -4,10 +4,13 @@ print("combining human evaluations..\n\n")
 
 evalPath = "../Human_Summaries/eval"
 mergePath = "../Human_Summaries"
+testPath = "../System_Summaries/test"
+SummaryPath = "../System_Summaries/submodular"
 
 
 def writeFile(file,text):
     print(os.path.join(mergePath, file))
+    text=text.replace('\n',"")
     f = open(os.path.join(mergePath, file),'a')
     f.write(text)
     f.close()
@@ -29,7 +32,24 @@ def readDocs(path):
 
     return docAsList
 
+def removeLines(path):
+    for doc in os.listdir(path):
+        docPath=os.path.join(path,doc)
+        with open(docPath) as file:
+            inText=file.read()
+            inText=inText.replace('\n',"")
+            outFile=doc.split('.')[0]
+            print(outFile)
+            f = open(os.path.join(testPath, outFile), 'w')
+            f.write(inText)
+            f.close()
+            file.close()
 
-readDocs(evalPath)
+
+
+#readDocs(evalPath)
+removeLines(SummaryPath)
+
+
 
 
